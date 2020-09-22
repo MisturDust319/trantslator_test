6e084c24e12078aad60d55ecbf66d1476dc2cf63
Stan S <sslup@hermetikos.com>
2020-09-22 18:42:00 -0400

    Defines all basic locators for project

--- locator.py
+++ locator.py
@@ -7,8 +7,47 @@ class MainPageLocators(object):
     """
     Define a class to hold all the locators for the main page
     """
-    # GO_BUTTON = (By.ID, "submit") # (how we access the element, what that value should be)
-    ANTVANCED_OPTIONS_BUTTON = (By.NAME, "ANTVANCED OPTIONS")
+    # text fields
+    INPUT_FIELD = (By.XPATH, "//div[@class='input']/textarea")
+    OUTPUT_FIELD = (By.XPATH, "//div[@class='output']/textarea")
+
+    # buttons
+    ANTVANCED_OPTIONS_BUTTON = (By.XPATH, "//div[@class='input']/button[1]")
+    TRANTSLATE_BUTTON = (By.XPATH, "//div[@class='input']/button[2]")
+    COPY_BUTTON = (By.XPATH, "//outbut[@class='input']/button")
+
+    # dropdowns
+    SPACING_DROPDOWN = (By.ID, 'spacing between words')
+    ANTEY_DROPDOWN = (By.ID, 'make it more ant-ey')
+
+    # text elements
+    SPACING_LABEL = (By.XPATH, "//div[@class='options']/label[1]")
+    ANTEY_LABEL = (By.XPATH, "//div[@class='options']/label[2]")
+
+class HeaderLocators(object):
+    # header
+    TITLE = (By.XPATH, "//header/h1")
+    ICON = (By.XPATH, "//header/*[name()='svg']")
+
+class FooterLocators(object):
+    # internal links
+    HR_1 = (By.XPATH, "//footer/hr[1]")
+    PAGES_HEADER = (By.XPATH, "//div[@class='footer-link'][1]/h3")
+    HOME_LINK = (By.XPATH, "//div[@class='footer-link'][1]/ul/li[1]/a")
+    HELP_LINK = (By.XPATH, "//div[@class='footer-link'][1]/ul/li[2]/a")
+    ABOUT_LINK = (By.XPATH, "//div[@class='footer-link'][1]/ul/li[3]/a")
+    
+    HR_2 = (By.XPATH, "//footer/hr[2]")
+
+    # external links
+    EXTERNAL_LINKS_HEADER = (By.XPATH, "//div[@class='footer-link'][2]/h3")
+    TWITTER_LINK = (By.XPATH, "//div[@class='footer-link'][2]/ul/li[1]/a")
+    TWITTER_SVG = (By.XPATH, "//div[@class='footer-link'][2]/ul/li[1]/a/*[name()='svg']")
+    INSTAGRAM_LINK = (By.XPATH, "//div[@class='footer-link'][2]/ul/li[2]/a")
+    INSTAGRAM_SVG = (By.XPATH, "//div[@class='footer-link'][2]/ul/li[2]/a/*[name()='svg']")
+    WEBSITE_LINK = (By.XPATH, "//div[@class='footer-link'][2]/ul/li[3]/a")
+    WEBSITE_SVG = (By.XPATH, "//div[@class='footer-link'][2]/ul/li[3]/a/*[name()='svg']")
+
 
 class SearchResultsPageLocators(object):
     """
