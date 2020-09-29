f526617852c8069482bae43b1e78310d39a280ae
Stan S <sslup@hermetikos.com>
2020-09-28 20:46:01 -0400

    Adds tests for main page header

--- src/main.py
+++ src/main.py
@@ -1,16 +1,18 @@
 import pytest
 from selenium import webdriver
+
 from page import MainPage
+from template import HeaderTemplate, FooterTemplate
 
 BASE_URL = "https://www.trantslator.com/"
 
 
 @pytest.fixture
-def set_up_driver():
+def set_up_page():
     """
     A fixture factory that will set up a driver in the specified page
     """
-    def _set_up_driver(url):
+    def _set_up_page(url):
         # create the webdriver object
         options = webdriver.FirefoxOptions()
         # set it to headless mode
@@ -20,16 +22,20 @@ def set_up_driver():
 
         # grab the site specified by url
         driver.get(url)
+        
+        # create the corresponding header and footer elements for the page
+        header = HeaderTemplate(driver)
+        footer = FooterTemplate(driver)
 
-        # return the driver
-        return driver
+        return driver, header, footer
     
-    return _set_up_driver
+    return _set_up_page
+
 
 @pytest.mark.home
-def test_home_page(set_up_driver):
-    # set up the driver
-    driver = set_up_driver(BASE_URL)
+def test_home_page(set_up_page):
+    # set up the driver, header, and footer
+    driver, header, footer = set_up_page(BASE_URL)
     # pass it to the Main Page object
     page = MainPage(driver)
 
@@ -37,3 +43,6 @@ def test_home_page(set_up_driver):
     assert page.getPageTitle() == "TRANTSLATOR | HOME"
 
     # test the header
+    assert header.check_for_header() is True, "Header not found"
+    assert header.check_header_title() is True, "Header title incorrect"
+    assert header.check_for_icon() is True, "Icon not found"
